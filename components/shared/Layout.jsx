034325978b9a8789d973526225e358b948b3d32d
mxkay <kayalyxvail@gmail.com>
2020-08-15 18:52:11 -0400

    Build header in layout

--- components/shared/Layout.jsx
+++ components/shared/Layout.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import { View, Platform, StatusBar } from 'react-native';
+import { Text, Header } from 'react-native-elements';
+import Octicons from "react-native-vector-icons/Octicons";
+
+const groupTitle = "House", supplyTitle = "Groceries"
 
 const Layout = ({ children }) => {
     return (
         <View style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} >
+            <Header containerStyle = {{ height: 60 }} >
+
+                {/* leftComponent */}
+                <></>
+
+                {/* centerComponent */}
+                <Text
+                    adjustsFontSizeToFit
+                    minimumFontScale={0.5}
+                    numberOfLines={1}
+                    style={{
+                        // fontSize: 20,
+                        fontWeight: 800,
+                        color: "#fff"
+                    }}
+                >
+                    {`${groupTitle} > ${supplyTitle}`}
+                </Text>
+
+                {/* rightComponent */}
+                <Octicons name="sign-out" size={30} color="#fff" />
+
+            </Header>
             {children}
         </View>
     );
