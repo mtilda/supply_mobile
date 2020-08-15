4ec0090181362e110861670f8fef0a810c576904
mxkay <kayalyxvail@gmail.com>
2020-08-15 17:11:06 -0400

    Fix statusbar overlap in layout

--- components/shared/Layout.jsx
+++ components/shared/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { View, Platform, StatusBar } from 'react-native';
 
 const Layout = ({ children }) => {
     return (
-        <>
+        <View style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} >
             {children}
-        </>
+        </View>
     );
 };
 
