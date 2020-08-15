74a994b0e3eb290bb28dfb11cf8fc2f9e21645ff
mxkay <kayalyxvail@gmail.com>
2020-08-15 16:46:12 -0400

    Add basic tab navigation

---
+++ components/ItemCreate.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Text } from "react-native"
+
+import Layout from "./shared/Layout.jsx"
+
+const ItemCreate = () => {
+    return (
+        <Layout>
+            <Text>ItemCreate</Text>
+        </Layout>
+    )
+}
+
+export default ItemCreate
