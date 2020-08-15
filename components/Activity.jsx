4d4136973fed5562e45445acc428b24f0fde30df
mxkay <kayalyxvail@gmail.com>
2020-08-15 16:46:12 -0400

    Added basic tab navigation

---
+++ components/Activity.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Text } from "react-native"
+
+import Layout from "./shared/Layout.jsx"
+
+const Activity = () => {
+    return (
+        <Layout>
+            <Text>Activity</Text>
+        </Layout>
+    )
+}
+
+export default Activity
---
+++ components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Text } from "react-native"
+
+import Layout from "./shared/Layout.jsx"
+
+const Home = () => {
+    return (
+        <Layout>
+            <Text>Home</Text>
+        </Layout>
+    )
+}
+
+export default Home
