ecd106de616622c35dbc683e5edb8a9f95b6a5d1
mxkay <kayalyxvail@gmail.com>
2020-08-17 16:35:30 -0400

    Built EventFeed skeleton

---
+++ components/shared/EventFeedItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const EventFeedItem = ({ event }) => {
+    console.log(event)
+    return (
+        <View>
+            <Text>Test</Text>
+        </View>
+    );
+}
+
+export default EventFeedItem;
