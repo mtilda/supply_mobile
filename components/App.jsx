61d1bed9281ea04cf0befcc095cab87856ccba58
mxkay <kayalyxvail@gmail.com>
2020-08-17 00:15:06 -0400

    Set up SessionDataContext

--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import Octicons from "react-native-vector-icons/Octicons";
 
 import Home from "./Home.jsx";
@@ -8,53 +9,74 @@ import Activity from "./Activity.jsx";
 import Inventory from "./Inventory.jsx";
 import ItemCreate from "./ItemCreate.jsx";
 
+import SessionDataContext from "./context/SessionDataContext"
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const [sessionData, setSessionData] = useState({
+    group:    { id: 1, name: "House", supplies: [] },
+    user:   { id: 1 },
+    supply: { id: null, name: "", color: "", items: [] },
+    item: {
+      id: null,
+      name: "",
+      unit: { symbol: "", singular: "", plural: "" },
+      isTracked: false,
+      isDepleted: false,
+      isRunningOut: false,
+      events: [],
+    }
+  });
+  const sessionDataValue = { sessionData, setSessionData };
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
+    <SessionDataContext.Provider value={sessionDataValue}>
+      <NavigationContainer>
+        <Tab.Navigator
 
-        screenOptions={({ route }) => ({
-          
-          tabBarIcon: ({ focused, color, size }) => {
+          screenOptions={({ route }) => ({
             
-            size = 40;
+            tabBarIcon: ({ focused, color, size }) => {
+              
+              size = 40;
 
-            let iconName;
+              let iconName;
 
-            switch(route.name) {
-              case "Supplies":
-                iconName = "versions"
-              break;
-              case "Activity":
-                iconName = "list-unordered";
-              break;
-              case "Inventory":
-                iconName = "checklist";
-              break;
-              case "New Item":
-                iconName = "diff-added";
-              break;
-              default:
+              switch(route.name) {
+                case "Supplies":
+                  iconName = "versions"
                 break;
-            }
+                case "Activity":
+                  iconName = "list-unordered";
+                break;
+                case "Inventory":
+                  iconName = "checklist";
+                break;
+                case "New Item":
+                  iconName = "diff-added";
+                break;
+                default:
+                  break;
+              }
 
-            return <Octicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: "tomato",
-          inactiveTintColor: "gray",
-          showLabel: false,
-          style: { height: 70 },
-        }}
-      >
-          <Tab.Screen name="Supplies" component={Home} options={{ size: 10 }} />
-          <Tab.Screen name="Activity" component={Activity} />
-          <Tab.Screen name="Inventory" component={Inventory} />
-          <Tab.Screen name="New Item" component={ItemCreate} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              return <Octicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+            showLabel: false,
+            style: { height: 70 },
+          }}
+        >
+            <Tab.Screen name="Supplies" component={Home} options={{ size: 10 }} />
+            <Tab.Screen name="Activity" component={Activity} />
+            <Tab.Screen name="Inventory" component={Inventory} />
+            <Tab.Screen name="New Item" component={ItemCreate} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SessionDataContext.Provider>
   )
 }
