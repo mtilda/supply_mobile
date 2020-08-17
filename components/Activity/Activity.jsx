7e17d3d54d30682673dfb53197aafca659099634
mxkay <kayalyxvail@gmail.com>
2020-08-17 16:35:03 -0400

    Set up API call and rendering EventFeed

--- components/Activity/Activity.jsx
+++ components/Activity/Activity.jsx
@@ -1,12 +1,40 @@
-import React from "react"
-import { Text } from "react-native"
+import React, { useState, useEffect, useContext } from "react";
+import { Text } from "react-native";
 
-import Layout from "../shared/Layout.jsx"
+import axios from "axios";
+
+import Layout from "../shared/Layout.jsx";
+import EventFeed from "../shared/EventFeed.jsx";
+
+import SessionDataContext from "../context/SessionDataContext";
 
 const Activity = () => {
+    const { sessionData, setSessionData } = useContext(SessionDataContext);
+    const [ events, setEvents ] = useState([]);
+    const [ activity, setActivity ] = useState(true);
+
+    // ON MOUNT
+    // FETCH supplies in current group
+    useEffect(() => {
+        const makeAPICall = async () => {
+            setActivity(true);
+            try {
+                const response = await axios({
+                    url: `https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supply.id}/events`,
+                    method: "GET"
+                });
+                setEvents(response.data);
+            } catch (err) {
+                console.error(err);
+            }
+            setActivity(false);
+        };
+        makeAPICall();
+    },[]);
+
     return (
         <Layout>
-            <Text>Activity</Text>
+            <EventFeed events={events} activity={activity} />
         </Layout>
     )
 }
