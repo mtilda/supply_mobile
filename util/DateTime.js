e4d6488202f4c5d5ad22687b6bafb7b10827e0b8
mxkay <kayalyxvail@gmail.com>
2020-08-18 00:00:07 -0400

    Added colors and date-time

---
+++ util/DateTime.js
@@ -0,0 +1,29 @@
+const dayName = (dayNumber) => {
+    switch(dayNumber) {
+        case 0: return "Sunday"; break;
+        case 1: return "Monday"; break;
+        case 2: return "Tuesday"; break;
+        case 3: return "Wednesday"; break;
+        case 4: return "Thursday"; break;
+        case 5: return "Friday"; break;
+        case 6: return "Saturday"; break;
+    }
+}
+
+const prettyDateTimeFromSQL = (SQLDateTime) => {
+
+    const date = new Date(SQLDateTime);
+    const timeFormatted = `${date.getHours()}:${date.getMinutes()}`;
+    const now = new Date();
+    let dateFormatted = "";
+
+    if( date.getTime() < now.getTime() && now - date < 604800000) {
+        dateFormatted = `${dayName(date.getDay())}`;
+    } else {
+        dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+
+    return timeFormatted + " on " + dateFormatted;
+};
+
+export { prettyDateTimeFromSQL };
