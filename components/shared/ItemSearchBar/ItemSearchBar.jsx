f72aea5ce6aa542e629e99d86231b802a8d2d696
mxkay <kayalyxvail@gmail.com>
2020-08-18 15:07:41 -0400

    Built ItemSearchBar

---
+++ components/shared/ItemSearchBar/ItemSearchBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View } from "react-native";
+import { Input } from "react-native-elements";
+
+import Octicons from "react-native-vector-icons/Octicons";
+
+const ItemSearchBar = ({ query, handleChange, handleSearch }) => {
+    return (
+        <View>
+            <Input
+                placeholder = "item"
+                onChangeText = { (text) => handleChange(text) }
+                value = { query }
+                rightIcon = {
+                    <Octicons name="search" size={20} color="black" onPress={() => handleSearch()} />
+                }
+            />
+        </View>
+    );
+}
+
+export default ItemSearchBar;
