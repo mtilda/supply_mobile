4f2a320686dfbe63c4919787d573ab3fe06b5ceb
mxkay <kayalyxvail@gmail.com>
2020-08-18 01:30:58 -0400

    Fixed scrollview for browser

--- components/shared/EventFeed.jsx
+++ components/shared/EventFeed.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { ActivityIndicator, View, Text } from 'react-native';
+import { ActivityIndicator, SafeAreaView, ScrollView, View, Text } from 'react-native';
 
 import EventFeedItem from "./EventFeedItem";
 
 const EventFeed = ({ events, units, activity }) => {
     return (
-        <View>
+        <SafeAreaView style={{ flex: 1 }}>
             {
                 activity ?
                     <ActivityIndicator />
                 :
-                    events[0]?
-                        units[0]?
-                            events.map( (event, index) => <EventFeedItem key={index} event={event} /> )
-                        :
-                            <Text>Failed to fetch units</Text>
-                    :
-                        units[0]?
-                            <Text>There has been no activity in this supply</Text>
-                        :
-                            <Text>Unable to fetch data from server</Text>
+                events[0]? units[0]?
+                    <ScrollView>
+                        { [...events, ...events].map( (event, index) => <EventFeedItem key={index} event={event} /> ) }
+                    </ScrollView>
+                :
+                    <Text>Failed to fetch units</Text>
+                : units[0]?
+                    <Text>There has been no activity in this supply</Text>
+                :
+                    <Text>Unable to fetch data from server</Text>
             }
-        </View>
+        </SafeAreaView>
     );
 }
 
