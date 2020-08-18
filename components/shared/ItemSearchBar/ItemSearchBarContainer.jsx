94fd6b7b56dcf60230197ee2fbdced2a19f0d712
mxkay <kayalyxvail@gmail.com>
2020-08-18 18:26:09 -0400

    Finish API calls

--- components/shared/ItemSearchBar/ItemSearchBarContainer.jsx
+++ components/shared/ItemSearchBar/ItemSearchBarContainer.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from "react"
-import { View } from "react-native"
+import React, { useState, useEffect, useContext } from "react";
+import { View } from "react-native";
+import axios from "axios";
 
 import SessionDataContext from "../../context/SessionDataContext";
 
@@ -9,18 +10,45 @@ const ItemSearchBarContainer = () => {
     const { sessionData, setSessionData } = useContext(SessionDataContext);
     const [ query, setQuery ] = useState("");
 
+    // ON MOUNT or CHANGE of group, supply
+    // FETCH items in current group, supply
+    useEffect( () => {
+        axios({
+            url: `https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supply.id}/items`,
+            method: "GET"
+        })
+        .then( (response) => {
+            console.log("FETCH ITEMS", response.data)
+            setSessionData({ ...sessionData, items: response.data })
+        })
+        .catch((error) => console.error(error));
+    },[sessionData.group, sessionData.supply]);
+
     const handleChange = (value) => {
         setQuery(value);
     }
 
+    // FIND item matching the current search query
+    // FETCH that item's data
     const handleSearch = async () => {
-        
-        axios.get(`https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supply.id}/items/${sessionData.item.id}`)
-        .then( (response) => {
-            setSessionData({ sessionData, item: response.data })
-            // setQuery("");
-        })
-        .catch((error) => console.error(error));
+        // console.log(sessionData.items);
+        const item = sessionData.items.find( (item) => item.name === query )
+        console.log(item);
+        console.log(item.id);
+        if( item ) {
+            axios({
+                url: `https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supplies.id}/items/${item.id}`,
+                method: "GET"
+            })
+            .then( (response) => {
+                console.log(response.data);
+                setSessionData({ ...sessionData, item: response.data });
+                // setQuery("");
+            })
+            .catch((error) => console.error(error));
+        } else {
+            console.error("item not found")
+        }
     }
 
     return <ItemSearchBar query={query} handleChange={handleChange} handleSearch={handleSearch} />
