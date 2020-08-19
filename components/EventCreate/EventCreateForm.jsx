83c56fb4361f2534e040f180a7ab56f0a1b658fe
mxkay <kayalyxvail@gmail.com>
2020-08-18 23:51:37 -0400

    Added styling

--- components/EventCreate/EventCreateForm.jsx
+++ components/EventCreate/EventCreateForm.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { ActivityIndicator, StyleSheet, View } from "react-native";
-import { Button, ButtonGroup, Text } from "react-native-elements";
+import { ActivityIndicator, ScrollView, StyleSheet, View } from "react-native";
+import { Button, ButtonGroup, Input, Text } from "react-native-elements";
 
 const EventCreateForm = ({ item, eventTypeIndex, handleEventTypeIndexChange, handleSubmit, activity }) => {
-    item.is_depleted = true;
 
     const eventTypeIndexButtons = [
         "GOT MORE",
@@ -20,47 +19,57 @@ const EventCreateForm = ({ item, eventTypeIndex, handleEventTypeIndexChange, han
 
     return activity ?
         <ActivityIndicator /> :
-        <View style={styles.container}>
-            { item && item.unit ?
-                <>
-                    <Text h2 style={styles.itemName} >{item.name}</Text>
-                    {!item.is_tracked ?
-                        <Text style={[ styles.statusBar, styles.untracked ]} >
-                            UNTRACKED
-                        </Text>
-                    : item.is_depleted ?
-                        <Text style={[ styles.statusBar, styles.depleted ]} >
-                            DEPLETED
-                        </Text>
-                    : item.is_running_out ?
-                        <Text style={[ styles.statusBar, styles.almsotOut ]} >
-                            ALMOST OUT
-                        </Text>
+        <View style={{ flex: 1 }}>
+            <ScrollView>
+                <View style={styles.container}>
+                    { item && item.unit ?
+                        <>
+                            <Text h2 style={styles.itemName} >{item.name}</Text>
+                            {!item.is_tracked ?
+                                <Text style={[ styles.statusBar, styles.untracked ]} >
+                                    UNTRACKED
+                                </Text>
+                            : item.is_depleted ?
+                                <Text style={[ styles.statusBar, styles.depleted ]} >
+                                    DEPLETED
+                                </Text>
+                            : item.is_running_out ?
+                                <Text style={[ styles.statusBar, styles.almsotOut ]} >
+                                    ALMOST OUT
+                                </Text>
+                            :
+                                <Text style={[ styles.statusBar, styles.inStock ]} >
+                                    IN STOCK
+                                </Text>
+                            }
+                            <ButtonGroup
+                                onPress={(selection) => {
+                                    handleEventTypeIndexChange(selection);
+                                }}
+                                selectedIndex={eventTypeIndex}
+                                buttons={eventTypeIndexButtons}
+                                disabled={disabledEventTypeIndexButtons}
+                                vertical={true}
+                                containerStyle={styles.buttonGroupContainer}
+                                buttonStyle={styles.buttonGroupButton}
+                                selectedButtonStyle={{backgroundColor: buttonColors[eventTypeIndex]}}
+                                disabledStyle={styles.buttonGroupDisabled}
+                            />
+                            { eventTypeIndex===0?
+                                <Input
+                                    label="How much?"
+                                />
+                            :<></>}
+                            <Button
+                                title="Submit"
+                                onPress={handleSubmit}
+                            />
+                        </>
                     :
-                        <Text style={[ styles.statusBar, styles.inStock ]} >
-                            IN STOCK
-                        </Text>
+                        <Text>Use the search bar to find an item</Text>
                     }
-                    <ButtonGroup
-                        onPress={(selection) => {
-                            handleEventTypeIndexChange(selection);
-                        }}
-                        selectedIndex={eventTypeIndex}
-                        buttons={eventTypeIndexButtons}
-                        disabled={disabledEventTypeIndexButtons}
-                        vertical={true}
-                        containerStyle={styles.buttonGroupContainer}
-                        buttonStyle={styles.buttonGroupButton}
-                        selectedButtonStyle={{backgroundColor: buttonColors[eventTypeIndex]}}
-                        disabledStyle={styles.buttonGroupDisabled}
-                    />
-                    <Button
-                        title="Submit"
-                    />
-                </>
-            :
-                <Text>Use the search bar to find an item</Text>
-            }
+                </View>
+            </ScrollView>
         </View>
     ;
 }
@@ -68,8 +77,8 @@ const EventCreateForm = ({ item, eventTypeIndex, handleEventTypeIndexChange, han
 const styles = StyleSheet.create({
     container: {
         flexDirection: "column",
-        padding: 5,
-        paddingHorizontal: 10,
+        height: "80%",
+        padding: 10,
     },
     itemName: {
         textTransform: "capitalize",
@@ -87,12 +96,7 @@ const styles = StyleSheet.create({
     almostOut: { backgroundColor: "coral" },
     inStock: { backgroundColor: "#00FA9A" },
     buttonGroupContainer: {
-        height: "100%"
-    },
-    buttonGroupButton: {
-
-    },
-    buttonGroupSelected: {
+        height: 300,
     },
     buttonGroupDisabled: {
         backgroundColor: "lightgrey",
