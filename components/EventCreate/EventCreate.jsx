afc8d181bdbad72ca33bf792a00c77b2e5b70444
mxkay <kayalyxvail@gmail.com>
2020-08-18 23:51:37 -0400

    Add styling

--- components/EventCreate/EventCreate.jsx
+++ components/EventCreate/EventCreate.jsx
@@ -12,9 +12,19 @@ const EventCreate = ({ navigation }) => {
     const { sessionData, setSessionData } = useContext(SessionDataContext);
     const [ eventData, setEventData ] = useState({
         dateTime: new Date(),
-        eventTypeIndex: null
+        eventTypeIndex: null,
+        delta: null,
     });
     const [ eventDataActivity, setEventDataActivity ] = useState(false);
+
+    const eventTable = [
+        "GET",
+        "DEPLETE",
+        "WARN",
+        "START",
+        "STOP"
+    ]
+
 
     const handleEventTypeIndexChange = (index) => {
         setEventData({ ...eventData, eventTypeIndex: index })
@@ -24,24 +34,33 @@ const EventCreate = ({ navigation }) => {
     // and permits { delta }
     const handleSubmit = async () => {
         setEventDataActivity(true);
-        await axios({
-            url: `https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supply.id}/items/${sessionData.item.id}`,
-            method: "POST",
-            data: {
-                date_time: dateTime,
-                event_type: eventTypeIndex,
-                user_id: sessionData.user.id,
-            },
-        })
-        .then(navigation.navigate('Profile'))
-        .catch(console.error);
+        // await axios({
+        //     url: `https://mxk-supply-api.herokuapp.com/groups/${sessionData.group.id}/supplies/${sessionData.supply.id}/items/${sessionData.item.id}/events`,
+        //     method: "POST",
+        //     data: {
+        //         date_time: eventData.dateTime,
+        //         event_type: eventTable[eventData.eventTypeIndex],
+        //         user_id: sessionData.user.id,
+        //         delta: eventData.eventTypeIndex === 0 ? eventData.delta : null
+        //     },
+        // })
+        // .then( (response) => {
+        //     console.log(response);
+        // })
+        // .catch(console.error);
         setEventDataActivity(false)
       };
 
     return (
         <Layout>
             <ItemSearchBarContainer />
-            <EventCreateForm item={sessionData.item} eventTypeIndex={eventData.eventTypeIndex} handleEventTypeIndexChange={handleEventTypeIndexChange} handleSubmit={handleSubmit} activity={eventDataActivity} />
+            <EventCreateForm
+                item={sessionData.item}
+                eventTypeIndex={eventData.eventTypeIndex}
+                handleEventTypeIndexChange={handleEventTypeIndexChange}
+                handleSubmit={handleSubmit}
+                activity={eventDataActivity}
+            />
         </Layout>
     );
 }
