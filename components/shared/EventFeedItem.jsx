fed47f2ecc578801d762ffb7a262009923b20699
mxkay <kayalyxvail@gmail.com>
2020-08-19 03:38:37 -0400

    Fixed unit display

--- components/shared/EventFeedItem.jsx
+++ components/shared/EventFeedItem.jsx
@@ -4,50 +4,50 @@ import { Text } from "react-native-elements";
 
 import { prettyDateTimeFromSQL } from "../../util/DateTime";
 
-const EventFeedItem = ({ event }) => (
-    <View style={styles.container}>
-        <View style={[styles.scarf, { backgroundColor:
-                event.event_type === "GET" ?            "lightgreen"
-            :   event.event_type === "DEPLETE" ?        "crimson"
-            :   event.event_type === "NEAR_DEPLETED" ?  "orange"
-            :   event.event_type === "START" ?          "aquamarine"
-            :   event.event_type === "STOP" ?           "mediumvioletred"
-            :                                           "purple"
-        }]}/>
-        <View style={styles.card} >
-            <Text style={[ styles.name ]} >{`${event.user.name}`}</Text>
-            {
-                event.event_type === "GET" ?
-                    <Text style={[ styles.message ]} >{`Picked up ${event.delta} ${
-                        event.delta == 1 ?
-                            event.item.unit.singular
-                        :
-                            event.item.unit.plural
-                        } of ${event.item.name}`}
-                    </Text>
+const EventFeedItem = ({ event }) => {
+    const plural = event.delta == 1 ? event.item.unit.singular : event.item.unit.plural;
 
-                : event.event_type === "DEPLETE" ?
-                    <Text style={[ styles.message ]} >{`We ran out of ${event.item.name}`}</Text>
+    return (
+        <View style={styles.container}>
+            <View style={[styles.scarf, { backgroundColor:
+                    event.event_type === "GET" ?            "lightgreen"
+                :   event.event_type === "DEPLETE" ?        "crimson"
+                :   event.event_type === "NEAR_DEPLETED" ?  "orange"
+                :   event.event_type === "START" ?          "aquamarine"
+                :   event.event_type === "STOP" ?           "mediumvioletred"
+                :                                           "purple"
+            }]}/>
 
-                : event.event_type === "NEAR_DEPLETED" ?
-                    <Text style={[ styles.message ]} >{`We're nearly out of ${event.item.name}`}</Text>
+            <View style={styles.card} >
+                <Text style={[ styles.name ]} >{`${event.user.name}`}</Text>
+                {
+                    event.event_type === "GET" ?
+                        <Text style={[ styles.message ]} >{`Picked up `}<Text style={{fontWeight: "bold"}}>{`${event.delta} ${plural ? plural + " of " : ""}${event.item.name}`}
+                        </Text></Text>
+
+                    : event.event_type === "DEPLETE" ?
+                        <Text style={[ styles.message ]} >{`We ran out of `}<Text style={{fontWeight: "bold"}}>{`${event.item.name}`}</Text></Text>
+
+                    : event.event_type === "NEAR_DEPLETED" ?
+                        <Text style={[ styles.message ]} >{`We're nearly out of `}<Text style={{fontWeight: "bold"}}>{`${event.item.name}`}</Text></Text>
+                    
+                    : event.event_type === "START" ?
+                        <Text style={[ styles.message ]} >{`Started tracking `}<Text style={{fontWeight: "bold"}}>{`${event.item.name}`}</Text></Text>
+                    
+                    : event.event_type === "STOP" ?
+                        <Text style={[ styles.message ]} >{`Stopped tracking `}<Text style={{fontWeight: "bold"}}>{`${event.item.name}`}</Text></Text>
                 
-                : event.event_type === "START" ?
-                    <Text style={[ styles.message ]} >{`Started tracking ${event.item.name}`}</Text>
-                
-                : event.event_type === "STOP" ?
-                    <Text style={[ styles.message ]} >{`Stopped tracking ${event.item.name}`}</Text>
-            
-                :
-                    <>
-                        <Text style={[ styles.message ]} >{`Did something unexpected`}</Text>
-                        {console.error(`unexpected event type: ${event.event_type}`)}
-                    </>
-            }
-            <Text style={[ styles.date ]} >{prettyDateTimeFromSQL(event.date_time)}</Text>
+                    :
+                        <>
+                            <Text style={[ styles.message ]} >{`Did something unexpected`}</Text>
+                            {console.error(`unexpected event type: ${event.event_type}`)}
+                        </>
+                }
+                <Text style={[ styles.date ]} >{prettyDateTimeFromSQL(event.date_time)}</Text>
+            </View>
         </View>
-    </View>
-);
+    )
+};
 
 const styles = StyleSheet.create({
     container: {
