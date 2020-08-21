c3066476ac470467bf57847bb0d22a23b40bcb08
mxkay <kayalyxvail@gmail.com>
2020-08-20 23:07:24 -0400

    Implement react-native-elements SearchBar

--- components/shared/ItemSearchBar/ItemSearchBar.jsx
+++ components/shared/ItemSearchBar/ItemSearchBar.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Input, Text } from "react-native-elements";
+import { Input, SearchBar, Text } from "react-native-elements";
 
 import Octicons from "react-native-vector-icons/Octicons";
 
-const ItemSearchBar = ({ query, message, handleChange, handleSearch }) => (
+const ItemSearchBar = ({ query, message, handleChange, handleCancel }) => (
     <View style={styles.container}>
-        <Input
-            placeholder = "item"
-            onChangeText = { (text) => handleChange(text) }
-            value = { query }
-            rightIcon = {
-                <Octicons name="search" size={20} color="black" onPress={() => handleSearch()} />
+        <SearchBar
+            platform={"default"}
+            placeHolder="search for an item...."
+            value={query}
+            onChangeText={handleChange}
+            onCancel={handleCancel}
+            searchIcon={
+                <Octicons name="search" size={20} color="black" />
             }
-            onSubmitEditing={() => handleSearch()}
+            clearIcon={
+                <Octicons name="x" size={30} color="rgba(0,0,0,0.5)" onPress={handleCancel} />
+            }
+            cancelIcon={<></>}
+            // lightTheme={true}
+            showCancel={false}
+            // round={true}
             autoCapitalize="none"
+            containerStyle={{ backgroundColor: "rgba(1,1,1,0)" }}
+            inputContainerStyle={{ backgroundColor: "white" }}
+            inputStyle={{ color: "black" }}
+            leftIconContainerStyle={{ paddingLeft: 5 }}
+            rightIconContainerStyle={{ paddingRight: 5 }}
         />
-        <Text style={[styles.message, { color: message.error ? "red" : "black" }]}>{message.content}</Text>
+        {/* <Text style={[styles.message, { color: message.error ? "red" : "black" }]}>{message.content}</Text> */}
     </View>
 );
 
 const styles = StyleSheet.create({
     container: {
-        padding: 10,
+        // padding: 10,
     },
     message: {
-        paddingHorizontal: 10,
-        height: 5,
+        // paddingHorizontal: 10,
+        // height: 5,
     }
 })
 
